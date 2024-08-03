8fd58e6f156813bc1f04e1563ee7ce5f864852a3
Senor-DPM <dhruvmukherjee85@gmail.com>
2024-08-03 20:05:17 +0530

    Added a couple key points.

--- 5analysis.py
+++ 5analysis.py
@@ -1,12 +1,14 @@
 import pandas as pd
 
 # head(x) method returns x number of rows from the top of the DataFrame along with the headings.
+# Default value of x is 5
 
 df = pd.read_csv("data.csv")
-print(df.head(5),"\n")
+print(df.head(10),"\n")
 
 # tail(x) method returns x number of rows from the bottom of the DataFrame along with the headings.
-print(df.tail(5),"\n")
+# Default value of x is 5
+print(df.tail(10),"\n")
 
 # info() method gives you more information about your data set.
 print(df.info())
